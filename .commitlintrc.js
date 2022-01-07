c402add713d56c7f99dd44a6f2f82aca6ae01c54
R_h_zero <renhongze@able-elec.com>
2022-01-07 15:44:16 +0800

    feat(config): 添加git钩子 校验提交 新增commit规范

---
+++ .commitlintrc.js
@@ -0,0 +1,64 @@
+// git commit 规范
+// <类型>[可选的作用域]: <描述>
+
+/* Commit message 包括三部分： Header, Body 和 Footer */
+
+/* Header 部分只有一行，三个字段： type（必需）、scope（可选）和subject （必需） */
+
+// # 主要type
+// feat:     增加新功能
+// fix:      修复bug
+
+// # 特殊type
+// docs:     只改动了文档相关的内容
+// style:    不影响代码含义的改动，例如去掉空格、改变缩进、增删分号
+// build:    构造工具的或者外部依赖的改动，例如webpack，npm
+// refactor: 代码重构时使用
+// revert:   执行git revert打印的message
+
+// # 暂不使用type
+// test:     添加测试或者修改现有测试
+// perf:     提高性能的改动
+// ci:       与CI（持续集成服务）有关的改动
+// chore:    不修改src或者test的其余修改，例如构建过程或辅助工具的变动
+
+// scope 用于说明 commit 影响的范围
+// init
+// runner
+// watcher
+// config
+// web-server
+// proxy
+// etc.
+
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'body-leading-blank': [2, 'always'],
+        'footer-leading-blank': [1, 'always'],
+        'header-max-length': [2, 'always', 108],
+        'subject-empty': [2, 'never'],
+        'type-empty': [2, 'never'],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'perf',
+                'style',
+                'docs',
+                'test',
+                'refactor',
+                'build',
+                'ci',
+                'chore',
+                'revert',
+                'wip',
+                'workflow',
+                'types',
+                'release'
+            ]
+        ]
+    }
+}
