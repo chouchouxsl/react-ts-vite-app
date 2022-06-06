3b7166dd248a5b70c3e1f001a039f93245e9a4dd
R_h_zero <renhongze@able-elec.com>
2022-06-06 18:44:11 +0800

    fix: xxxx

--- src/hooks/useLocale.ts
+++ src/hooks/useLocale.ts
@@ -1,11 +1,11 @@
 import { useContext } from 'react'
-import { GlobalContext } from '@/context/globalContext'
+import { GlobalContext, IGlobalContent } from '@/context/globalContext'
 import defaultLocale from '../locale'
 
 function useLocale(locale = null) {
-    const { lang } = useContext(GlobalContext)
+    const { lang } = useContext(GlobalContext) as Required<IGlobalContent>
 
-    if (lang) return (locale || defaultLocale)[lang] || {}
+    return (locale || defaultLocale)[lang] || {}
 }
 
 export default useLocale
