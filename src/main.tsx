b96115f8ac3fe3897fa0f58b40143fee586a711f
R_h_zero <renhongze@able-elec.com>
2022-10-27 10:57:38 +0800

    perf: extract particles code

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { ConfigProvider, Spin } from '@arco-design/web-react'
 import Particles from 'react-tsparticles'
 import zhCN from '@arco-design/web-react/es/locale/zh-CN'
 import enUS from '@arco-design/web-react/es/locale/en-US'
+import { particlesOption } from '@/utils/particles-config'
 import { AuthRoute, history } from '@/route'
 import store from '@/redux'
 import PageLayout from '@/layout'
@@ -60,65 +61,7 @@ const Index: React.FC = () => {
                 <ConfigProvider locale={getLocal()}>
                     <Provider store={store}>
                         <GlobalContext.Provider value={contextValue}>
-                            <Particles
-                                options={{
-                                    backgroundMode: {
-                                        enable: true,
-                                        zIndex: 0
-                                    },
-                                    fpsLimit: 60,
-                                    interactivity: {
-                                        detectsOn: 'canvas',
-                                        events: {
-                                            onClick: { enable: true, mode: 'repulse' },
-                                            onHover: {
-                                                enable: true,
-                                                mode: 'bubble',
-                                                parallax: { enable: false, force: 2, smooth: 10 }
-                                            },
-                                            resize: true
-                                        },
-                                        modes: {
-                                            bubble: {
-                                                distance: 400,
-                                                duration: 0.3,
-                                                opacity: 1,
-                                                size: 4
-                                            },
-                                            grab: { distance: 400, line_linked: { opacity: 0.5 } },
-                                            push: { particles_nb: 4 },
-                                            remove: { particles_nb: 2 },
-                                            repulse: { distance: 200, duration: 0.4 }
-                                        }
-                                    },
-                                    particles: {
-                                        color: { value: theme === ThemeEnum.LIGHT ? '#000' : '#fff' },
-                                        move: {
-                                            attract: { enable: false, rotateX: 600, rotateY: 1200 },
-                                            direction: 'bottom-left',
-                                            enable: true,
-                                            outMode: 'out',
-                                            random: false,
-                                            size: true,
-                                            speed: 1,
-                                            straight: false
-                                        },
-                                        number: { density: { enable: true, area: 800 }, value: 400 },
-                                        opacity: {
-                                            random: true,
-                                            value: 0.5
-                                        },
-                                        shape: {
-                                            type: 'star'
-                                        },
-                                        size: {
-                                            random: true,
-                                            value: 10
-                                        }
-                                    },
-                                    detectRetina: true
-                                }}
-                            />
+                            <Particles options={particlesOption(theme as ThemeEnum)} />
                             <Switch>
                                 <Route path="/login" component={Login} />
                                 <AuthRoute path="/" component={PageLayout} />
